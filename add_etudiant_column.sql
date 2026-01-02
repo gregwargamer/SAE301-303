c395b261291eab8b2975ab713ec7a80ebcc45b8d
GregOire <gregwargamer@gmail.com>
2026-01-02 13:43:44 +0100

    plus de cloudflare parce que je comprend rien + nettoyage

--- add_etudiant_column.sql
+++
@@ -1,7 +0,0 @@
--- Ajouter la colonne etudiant à la table users si elle n'existe pas
--- Cette colonne permet de tracker le pourcentage d'étudiants inscrits
-
-ALTER TABLE users ADD COLUMN etudiant TINYINT(1) DEFAULT 0;
-
--- Mettre à jour les utilisateurs existants qui ont coché "étudiant" lors de l'inscription
--- Par défaut, tous les utilisateurs existants sont marqués comme non-étudiants
