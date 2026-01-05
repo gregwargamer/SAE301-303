8e3fabc5bc087eabffff956f283e523baae4f135
AsMool <mthdlt@gmail.com>
2026-01-05 19:16:22 +0100

    formulaire de contact

--- sae/src/app/home-component/home-component.ts
+++ sae/src/app/home-component/home-component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { RestApiService } from '../services/rest-api.service';
+import { ConfigService } from '../services/config.service';
 
 interface Box {
   id: string;
@@ -16,15 +19,30 @@ interface Box {
 
 @Component({
   selector: 'app-home-component',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
 })
 export class HomeComponent implements OnInit {
   nouveautes: Box[] = [];
   bestSellers: Box[] = [];
+  
+  // Formulaire de contact
+  contactForm = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
+  isSubmitting = false;
+  contactMessage = '';
+  contactSuccess = false;
 
-  constructor(private restApiService: RestApiService) { }
+  constructor(
+    private restApiService: RestApiService,
+    private http: HttpClient,
+    private config: ConfigService
+  ) { }
 
   ngOnInit(): void {
     // Récupère les 3 derniers produits ajoutés (nouveautés)
@@ -36,5 +54,45 @@ export class HomeComponent implements OnInit {
     this.restApiService.getBestSellers().subscribe((data: Box[]) => {
       this.bestSellers = data;
     });
+  }
+
+  onSubmitContact(): void {
+    if (this.isSubmitting) return;
+
+    // Validation basique
+    if (!this.contactForm.name || !this.contactForm.email || !this.contactForm.subject || !this.contactForm.message) {
+      this.contactMessage = 'Veuillez remplir tous les champs.';
+      this.contactSuccess = false;
+      return;
+    }
+
+    this.isSubmitting = true;
+    this.contactMessage = '';
+
+    this.http.post(`${this.config.apiBase}/contact/index.php`, this.contactForm)
+      .subscribe({
+        next: (response: any) => {
+          this.contactSuccess = true;
+          this.contactMessage = 'Votre message a été envoyé avec succès !';
+          // Réinitialiser le formulaire
+          this.contactForm = {
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+          };
+          this.isSubmitting = false;
+          
+          // Effacer le message après 5 secondes
+          setTimeout(() => {
+            this.contactMessage = '';
+          }, 5000);
+        },
+        error: (error) => {
+          this.contactSuccess = false;
+          this.contactMessage = 'Une erreur est survenue. Veuillez réessayer.';
+          this.isSubmitting = false;
+        }
+      });
   }
 }
